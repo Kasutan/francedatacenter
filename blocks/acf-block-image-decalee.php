66d38f43eb2a7d450db4d890c2d74e6954574811
Kasutan <contact@kasutan.pro>
2020-07-26 20:07:50 +0200

    Ajout d'une variante au bloc image décalé

--- blocks/acf-block-image-decalee.php
+++ blocks/acf-block-image-decalee.php
@@ -6,8 +6,8 @@ function fdc_acf_block_image_decalee_acf_init() {
 	if ( function_exists( 'acf_register_block_type' ) ) {
 		acf_register_block_type( [
 			'name'            => 'acf-image-decalee',
-			'title'           => 'Bloc image décalée',
-			'description'     => 'Bloc avec image flottante décalée à droite et décor carré bleu transparent au-dessus de l\'image',
+			'title'           => 'Bloc image avec décor',
+			'description'     => 'Bloc avec image et décor carré bleu transparent au-dessus de l\'image, option pour décaler  l\'image à droite',
 			'render_callback' => 'fdc_image_decalee_callback',
 			'category'        => 'francedatacenter',
 			'icon'            => 'format-image',
@@ -30,10 +30,14 @@ function fdc_image_decalee_callback( $block ) {
 		$className=esc_attr($block["className"]);
 	} else $className='';
 
+	$style=esc_attr(get_field('style'));
+	if(!$style) {
+		$style='decalee';
+	}
 
 	$image_id=esc_attr( get_field('image') );
 	if($image_id) :
-		printf('<div class="acf-block-image-decalee %s">', $className);
+		printf('<div class="acf-block-image-decalee %s %s">', $className, $style);
 			printf('<div class="image">%s<div class="decor"></div></div>',	wp_get_attachment_image( $image_id ));
 		echo "</div>";
 	endif;
